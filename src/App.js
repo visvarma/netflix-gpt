88c4e09f3f79ec04c9594fdc9f4deea2d96611af
visvarma <visvarma2021@gmail.com>
2024-01-13 20:31:25 +0530

    CRA & Tailwind Config

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="text-3xl font-semibold text-red-400">
+      Lets build <span className="font-bold">Netflix-GPT</span>
+    </div>
+  );
+}
+
+export default App;
